a1c4c69ced76134409ab661a8d5fa1907098d939
Manishakahar <manisha.kahar1998@gmail.com>
2021-12-06 01:03:25 +0530

    Added: Update the contact Details.

--- src/com/blz/address/AddressBookMain.java
+++ src/com/blz/address/AddressBookMain.java
@@ -3,78 +3,38 @@ package com.blz.address;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-// Declaring Variable
 public class AddressBookMain {
-    String firstName;
-    String lastName;
-    String city;
-    String state;
-    String address;
-    String emailId;
-    long phoneNo;
-    int zipCode;
-    ArrayList<AddressBookMain> contactDetails=new ArrayList<>();
-    String userWish="y";
-    int count=1;
 
-    // Creating function to get user Input
-    private void getInput() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the First Name : ");
-        firstName = sc.next();
-
-        System.out.print("Enter the Last Name : ");
-        lastName = sc.next();
-
-        System.out.print("Enter the Address: ");
-        address = sc.next();
-
-        System.out.print("Enter the City Name : ");
-        city = sc.next();
-
-        System.out.print("Enter the state Name : ");
-        state = sc.next();
-
-        System.out.print("Enter the Email-Id : ");
-        emailId = sc.next();
-
-        System.out.print("Enter the Zip Code : ");
-        zipCode = sc.nextInt();
-
-        System.out.print("Enter the  Phone Number: ");
-        phoneNo = sc.nextLong();
-    }
-       // Display User Details
-    private void displayDetails(AddressBookMain display){
-        System.out.println("contact"+count);
-        System.out.println("First Name: " + firstName);
-        System.out.println("Last Name: " + lastName);
-        System.out.println("Address: " + address);
-        System.out.println("City Name : " + city);
-        System.out.println("State Name : " + state);
-        System.out.println("Email-Id : " + emailId);
-        System.out.println("Zip Code : " + zipCode);
-        System.out.println("phone Number Name : " + phoneNo);
-        count++;
-    }
-
-    // Main Method
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
-        AddressBookMain addressBookMain=new AddressBookMain();
-        Scanner userInput =new Scanner(System.in);
-        while(addressBookMain.userWish.matches("y")) {
-            addressBookMain.getInput();
-            addressBookMain.contactDetails.add(addressBookMain);
-            System.out.println("IF YOU WANT TO ADD NEW RECORD PLEASE INSERT " + "y " + "OR else " + "n");
-            addressBookMain.userWish = userInput.next();
-            if (addressBookMain.userWish == "n")
-            {
-                break;
+        ContactPersonDetails contactPersonDetails = new ContactPersonDetails();
+        ArrayList<ContactPersonDetails> contactDetails = new ArrayList<>();
+        String name;
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
+        int userChoice = 1;
+        // User Choose the Option
+        while (flag) {
+            System.out.print(" 1 - Add more contact \n2 - Edit Contact \n3 - Show AddressBook " + "\n0 - for exist \n Enter your choice");
+            userChoice = sc.nextInt();
+
+            switch (userChoice) {
+                case 1:
+                    contactDetails.add(contactPersonDetails.getInput());
+                    break;
+                case 2:
+                    System.out.println("Enter first name that you want to edit contactDetails");
+                    name = sc.next();
+                    contactPersonDetails.updateData(name, contactDetails);
+                    break;
+                case 3:
+                    System.out.println("contactDetails : " + contactDetails.size());
+                    contactPersonDetails.addressBook(contactDetails);
+                    break;
+                case 0:
+                    flag = false;
+                    break;
             }
-        }
-        for (int i = 0; i < addressBookMain.contactDetails.size(); i++) {
-            addressBookMain.displayDetails(addressBookMain.contactDetails.get(i));
         }
     }
 }
---
+++ src/com/blz/address/ContactPersonDetails.java
@@ -0,0 +1,78 @@
+package com.blz.address;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// Declaring Variable
+public class ContactPersonDetails {
+    String firstName;
+    String lastName;
+    String city;
+    String state;
+    String address;
+    String emailId;
+    long phoneNo;
+    int zipCode;
+    int count = 1;
+
+    public void ContactPerson() {
+    }
+
+    // Display User Details
+    public void addressBook(ArrayList<ContactPersonDetails> contactPerson) {
+        for (int i = 0; i < contactPerson.size(); i++) {
+            System.out.println("contact" + count);
+            System.out.println("First Name: " + firstName);
+            System.out.println("Last Name: " + lastName);
+            System.out.println("Address: " + address);
+            System.out.println("City Name : " + city);
+            System.out.println("State Name : " + state);
+            System.out.println("Email-Id : " + emailId);
+            System.out.println("Zip Code : " + zipCode);
+            System.out.println("phone Number Name : " + phoneNo);
+            count++;
+        }
+    }
+
+    //          Update the Contact Details
+    public void updateData(String name, ArrayList<ContactPersonDetails> contactDetails) {
+        for (int i = 0; i < contactDetails.size(); i++) {
+            if (name.equals(contactDetails.get(i).firstName)) {
+                contactDetails.remove(i);
+                contactDetails.add(i, getInput());
+            }
+        }
+        System.out.println("Record Update Successfully.......");
+    }
+
+    // Creating function to get user Input
+
+    public ContactPersonDetails getInput() {
+        ContactPersonDetails addressBookMain = new ContactPersonDetails();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the First Name : ");
+        addressBookMain.firstName = sc.next();
+
+        System.out.print("Enter the Last Name : ");
+        addressBookMain.lastName = sc.next();
+
+        System.out.print("Enter the Address: ");
+        addressBookMain.address = sc.next();
+
+        System.out.print("Enter the City Name : ");
+        addressBookMain.city = sc.next();
+
+        System.out.print("Enter the state Name : ");
+        addressBookMain.state = sc.next();
+
+        System.out.print("Enter the Email-Id : ");
+        addressBookMain.emailId = sc.next();
+
+        System.out.print("Enter the Zip Code : ");
+        addressBookMain.zipCode = sc.nextInt();
+
+        System.out.print("Enter the  Phone Number: ");
+        addressBookMain.phoneNo = sc.nextLong();
+        return addressBookMain;
+    }
+}
